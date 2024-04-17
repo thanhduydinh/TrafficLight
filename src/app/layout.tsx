55e37e1b329e8ad4f811e9e6a897a2ef75c2822f
lam.ho <lam.ho@sotatek.com>
2024-04-17 15:17:22 +0700

    update code base

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import "./globals.css";
+import "./globals.scss";
 
 const inter = Inter({ subsets: ["latin"] });
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { cn } from "../config/utils";
 import { getTodos } from "../services";
 import Count2 from "./_components/Count";
 
@@ -6,7 +7,12 @@ export default async function Home() {
   console.log("data: ", todo);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main
+      className={cn(
+        "flex min-h-screen flex-col items-center justify-between",
+        todo?.length > 0 && "p-24"
+      )}
+    >
       <Count2 />
       TODOs
       <div>
